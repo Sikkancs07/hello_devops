80c4cd209267e33bf4a535636a1ea318586ff826
Attila Sikes <attila.sikes07@gmail.com>
2025-12-08 01:03:42 +0100

    repo rename test with welcome message change

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -22,7 +22,7 @@ export function Welcome() {
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              Hello Devops,  this is the final message !
+              Hello Devops,  this is the final message !!
             </p>
           </nav>
         </div>
